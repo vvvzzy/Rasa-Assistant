5e3acf0aac90fa4fea97a331421e07395e27d914
vvvzzy <3314927425@qq.com>
2026-06-23 22:30:01 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -449,4 +449,4 @@ def chat():
 
 if __name__ == "__main__":
     port = int(os.getenv("FLASK_PORT", "8000"))
-    app.run(host="0.0.0.0", port=port, debug=True)
+    app.run(host="0.0.0.0", port=port) # , debug=True
